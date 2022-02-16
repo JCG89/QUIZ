cd25b88dd03701017d00799a009769fea081a682
Jean Christophe Gomis <devdujour@yahoo.com>
2022-02-16 23:52:38 +0100

    ajout des alertes

--- main.js
+++ main.js
@@ -2,19 +2,25 @@ const box = document.querySelector('.box');
 const vrais = document.querySelectorAll('.vrai');
 const faux = document.querySelectorAll('.faux');
 const resp = document.querySelectorAll('.result');
+console.log(resp)
 
 
 vrais.forEach(vrai => {
 
       vrai.addEventListener('click', () => {
             vrai.classList.add('juste');
+            alert('Bonne réponse')
       })
 })
 faux.forEach(fau => {
       fau.addEventListener('click', () => {
             fau.classList.add('injuste');
+            alert('Mauvaise réponse!')
       })
 })
+
+
+
 
 
 
